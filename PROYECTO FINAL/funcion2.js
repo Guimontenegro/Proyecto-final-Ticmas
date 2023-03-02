c1173a047a7a22ad9a7c07b4b93691c811b0c41e
Guido Montenegro <guidoprograma1987@gmail.com>
2023-03-01 23:55:40 -0300

    PARA ENTREGAR

--- PROYECTO FINAL/funcion2.js
+++ PROYECTO FINAL/funcion2.js
@@ -1,19 +1,36 @@
 
 // Obtener los elementos del DOM
-const nombre = document.getElementById('Guido Montenegro');
-const edad = document.getElementById('35');
-const correo = document.getElementById('guidoprograma1987@gmail.com');
-const telefono = document.getElementById('2932520271');
-const habilidades = document.getElementById('Creatividad,Adaptabilidad,Colaboración,Negociación');
+let nombre = document.getElementById('Nombre');
+let edad = document.getElementById('Edad');
+let correo = document.getElementById('Correo');
+let telefono = document.getElementById('Telefono');
+let habilidades = document.getElementById('Habilidades');
 
 // Actualizar los elementos del DOM con la información del currículum vitae
 nombre.textContent = 'Guido Montenegro';
 edad.textContent = 'Edad: 35';
-correo.textContent = 'Correo electrónico: guidoprograma1987@gmail.com';
+correo.textContent = 'Correo electrónico: nombre.apellido@gmail.com';
 telefono.textContent = 'Teléfono: (2932) 520271';
-habilidades.textContent = 'Habilidades: HTML (básico),CSS (básico),JavaScript (básico)';
+habilidades.textContent = 'Habilidades: HTML, CSS, JavaScript (básico)';
 
-// Agregar una clase activa al enlace de navegación de currículum
-const enlaceCurriculum = document.getElementById('enlace-curriculum');
+function scaleCv(){
+    document.body.classList.add('scale-cv')
+};
+function removeScale(){
+    document.body.classList.remove('scale-cv')
+}
+let areaCv=document.getElementById('area-cv');
+let boton =document.getElementById('boton-pdf');
+function generateResume(){
+    html2pdf(areaCv, opt)
+};
 
-enlaceCurriculum.classList.add('activo');
+
+let opt = {
+  margin:       0,
+  filename:     'documento.pdf',
+  image:        { type: 'jpeg', quality: 0.98 },
+  html2canvas:  { scale: 4 },
+  jsPDF:        { format: 'a4', orientation: 'portrait' }
+};
+
