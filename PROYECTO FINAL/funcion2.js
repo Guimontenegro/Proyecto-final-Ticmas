e225aebe4179f3923927553be8213683d2be8f03
Guido Montenegro <guidoprograma1987@gmail.com>
2023-03-07 18:03:20 -0300

    Cambios finales

--- PROYECTO FINAL/funcion2.js
+++ PROYECTO FINAL/funcion2.js
@@ -9,28 +9,31 @@ let habilidades = document.getElementById('Habilidades');
 // Actualizar los elementos del DOM con la información del currículum vitae
 nombre.textContent = 'Guido Montenegro';
 edad.textContent = 'Edad: 35';
-correo.textContent = 'Correo electrónico: nombre.apellido@gmail.com';
+correo.textContent = 'Correo electrónico: guidoprograma1987@gmail.com';
 telefono.textContent = 'Teléfono: (2932) 520271';
 habilidades.textContent = 'Habilidades: HTML, CSS, JavaScript (básico)';
+/**AREA PARA GENERAR PDF */
+let areaCv = document.getElementById('area-cv')
 
-function scaleCv(){
-    document.body.classList.add('scale-cv')
-};
-function removeScale(){
-    document.body.classList.remove('scale-cv')
-}
-let areaCv=document.getElementById('area-cv');
-let boton =document.getElementById('boton-pdf');
+
+/**FUNCION PARA LLAMAR A AREACV Y HTML2PDF */
 function generateResume(){
-    html2pdf(areaCv, opt)
-};
+    html2pdf(areaCv,opt);
+}
+resumeButton.addEventListener;{('click',() => {
+    scaleCv(),
+    generateResume();
+})};
 
-
-let opt = {
+generateResume()
+var opt = {
   margin:       0,
   filename:     'documento.pdf',
   image:        { type: 'jpeg', quality: 0.98 },
   html2canvas:  { scale: 4 },
-  jsPDF:        { format: 'a4', orientation: 'portrait' }
-};
+  jsPDF:        { format: 'a4', orientation: 'portrait' },
+}
+
+
+
 
